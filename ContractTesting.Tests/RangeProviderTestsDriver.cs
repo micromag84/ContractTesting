b9a5d831d415e94969e0ce472b0e907044ed0fb6
Girolamo Marroccoli <micromag84@users.noreply.github.com>
2017-09-16 19:39:34 +0200

    testing implementations hardcoded and by reflection

---
+++ ContractTesting.Tests/RangeProviderTestsDriver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using ContractTesting.Core;
+using ContractTesting.Core.Interfaces;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ContractTesting.Tests
+{
+    [TestFixture]
+    public class RangeProviderTestsDriver
+    {
+        [Test]
+        public void AllImplementations_ValidRangeShouldBeBetween10And25()
+        {
+            IList<IDataRangeProvider> providersToTest = new List<IDataRangeProvider>();
+            providersToTest.Add(new FileDataRangeProvider());
+            providersToTest.Add(new StorageDataRangeProvider());
+
+            foreach (var provider in providersToTest)
+                provider.GetValidRange().Should().BeInRange(10, 25);
+        }
+    }
+}
---
+++ ContractTesting.Tests/RangeProviderTestsDriverWithReflection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ContractTesting.Core.Interfaces;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace ContractTesting.Tests
+{
+    [TestFixture]
+    public class RangeProviderTestsDriverWithReflection
+    {
+        [Test]
+        public void AllImplementations_ValidRangeShouldBeBetween10And25()
+        {
+            var interfaceType = typeof(IDataRangeProvider);
+
+            IList<IDataRangeProvider> providersToTest = new List<IDataRangeProvider>();
+
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.IsClass && interfaceType.IsAssignableFrom(t));
+
+            foreach (var type in types)
+            {
+                var provider = (IDataRangeProvider) Activator.CreateInstance(type);
+                provider.GetValidRange().Should().BeInRange(10, 25);
+            }
+        }
+    }
+}
