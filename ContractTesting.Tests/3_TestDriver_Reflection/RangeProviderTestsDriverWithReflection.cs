0b0fe4d05cdebabc49700068e69b8cb497490f8d
Girolamo Marroccoli <micromag84@users.noreply.github.com>
2017-09-18 10:37:05 +0200

    Introduced AssertAllImplementationsOf<T>

--- ContractTesting.Tests/3_TestDriver_Reflection/RangeProviderTestsDriverWithReflection.cs
+++ ContractTesting.Tests/3_TestDriver_Reflection/RangeProviderTestsDriverWithReflection.cs
@@ -9,20 +9,27 @@ namespace ContractTesting.Tests._3_TestDriver_Reflection
     [TestFixture]
     public class RangeProviderTestsDriverWithReflection
     {
+        private void AssertAllImplementationsOf<T>(Action<T> action)
+        {
+            var types = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(a => a.GetTypes())
+                .Where(t => t.IsClass && typeof(T).IsAssignableFrom(t)).ToList();
+
+            types.ForEach(type =>
+            {
+                var instance = (IDataRangeProvider) Activator.CreateInstance(type);
+                action((T) instance);
+            });
+        }
+
+
         [Test]
         public void AllImplementations_ValidRangeShouldBeBetween10And25()
         {
-            var interfaceType = typeof(IDataRangeProvider);
-
-            var types = AppDomain.CurrentDomain.GetAssemblies()
-                .SelectMany(a => a.GetTypes())
-                .Where(t => t.IsClass && interfaceType.IsAssignableFrom(t));
-
-            foreach (var type in types)
+            AssertAllImplementationsOf<IDataRangeProvider>(provider =>
             {
-                var provider = (IDataRangeProvider) Activator.CreateInstance(type);
-                provider.GetValidRange().Should().BeInRange(10, 25, $"of type '{type.Name}'");
-            }
+                provider.GetValidRange().Should().BeInRange(10, 25, $"of type '{provider.GetType().Name}'");
+            });
         }
     }
 }
